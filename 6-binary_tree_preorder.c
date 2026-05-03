2ec451292bab8087fe13f06dd69b43bdacc6a6de
MN7 <you@example.com>
2026-05-03 14:27:34 +0300

    Holberton Task

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+* binary_tree_preorder - entry point
+* @node: string to generate the real key for dictionary
+* @func: pointer function
+* Return: always (0) success
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree == NULL || func == NULL)
+{
+return;
+}
+
+func(tree->n);
+binary_tree_preorder(tree->left, func);
+binary_tree_preorder(tree->right, func);
+    
+}
