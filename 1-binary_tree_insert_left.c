5720764df8651ff32b5d3553fbd2cdb213606c51
MN7 <you@example.com>
2026-05-03 13:00:24 +0300

    Holberton Task

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+* binary_tree_insert_left - entry point
+* @parent: string to generate the real key for dictionary
+* @value: value
+* Return: always (0) success
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *node;
+if (parent == NULL)
+{
+return (NULL);
+}
+node = malloc(sizeof(binary_tree_t));
+if (node == NULL)
+{
+free(node);
+return (NULL);
+}
+node->n = value;
+node->parent = NULL;
+node->left = NULL;
+node->right = NULL;
+
+if (parent->left != NULL)
+{
+node->parent = parent->left;
+}
+
+
+parent->left = node;
+
+return (node);
+}
