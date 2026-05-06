2aa30aff3f9db2e25826cfb6b2482450a1711019
MN7 <you@example.com>
2026-05-06 11:57:18 +0300

    advanced

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+* binary_tree_is_complete - entry point
+* @tree: string to generate the real key for dictionary
+* Return: always (0) success
+*/
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+if (tree == NULL)
+{
+return (0);
+}
+
+if (binary_tree_depth(tree->left) == binary_tree_depth(tree->right))
+{
+return (1);
+}
+}
