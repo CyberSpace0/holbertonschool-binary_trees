8ef2a96055de3ae6e537f15c4ac4e3f2020a0e0c
MN7 <you@example.com>
2026-05-06 12:02:14 +0300

    advanced

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdio.h>
 
+size_t binary_tree_depth(const binary_tree_t *tree);
+
 
 /**
 * binary_tree_is_complete - entry point
@@ -21,4 +23,25 @@ if (binary_tree_depth(tree->left) == binary_tree_depth(tree->right))
 return (1);
 }
 return (0);
+}
+
+
+/**
+ * binary_tree_depth - Measures the depth of a node in a binary tree
+ * @tree: Pointer to the node to measure the depth
+ *
+ * Return: Depth of the node, or 0 if tree is NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t counter = 1;
+
+	if (!tree || !tree->parent)
+	{
+		return (0);
+	}
+	else
+		counter += binary_tree_depth(tree->parent);
+
+	return (counter);
 }
